4e0162e371d5c80ac7e61533655175680d7bd5e9
guzmanem <guzmanem@hotmail.com>
2019-01-30 13:49:45 -0300

    Cambio de esquema

--- app_entrega/views.py
+++ app_entrega/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+
+
+#def index(request):
+#    template = loader.get_template('app_entrega/index.html')
+#    context={}
+#    return HttpResponse(template.render(context,request))
 
 def index(request):
-    template = loader.get_template('app_entrega/index.html')
-    context={}
-    return HttpResponse(template.render(context,request))
+    return render(request, 'app_entrega/index.html', {})
