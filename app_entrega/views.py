d9b40931ecec788388f04f56e3f139bf6838bafb
guzmanem <guzmanem@hotmail.com>
2019-01-30 18:03:45 -0300

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- app_entrega/views.py
+++ app_entrega/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 
 #def index(request):
@@ -13,3 +14,7 @@ from .models import Post
 def index(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'app_entrega/index.html', {'posts': posts}) 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'app_entrega/post_detail.html', {'post': post})
